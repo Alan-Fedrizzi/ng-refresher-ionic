0ed92a76328ad570beb95c5c2fce9cd42fb22ad8
alanfedrizzi <alanfedrizzi@gmail.com>
2023-03-27 21:01:52 -0300

    busget functionalitys beginning

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import { HeaderComponent } from './header/header.component';
 import { CharactersComponent } from './characters.component/characters.component';
 import { BudgetComponent } from './budget.component/budget.component';
 import { CoursesComponent } from './courses.component/courses.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -19,7 +20,12 @@ import { CoursesComponent } from './courses.component/courses.component';
     BudgetComponent,
     CoursesComponent,
   ],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(),
+    AppRoutingModule,
+    FormsModule,
+  ],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
--- src/app/budget.component/budget.component.ts
+++ src/app/budget.component/budget.component.ts
@@ -6,5 +6,27 @@ import { Component } from '@angular/core';
   styleUrls: ['budget.component.scss'],
 })
 export class BudgetComponent {
+  enteredReason: string;
+  enteredAmount: string;
+
   constructor() {}
+
+  onClearInputs() {
+    this.enteredReason = '';
+    this.enteredAmount = '';
+  }
+
+  onAddExpense() {
+    if (!this.enteredReason || !this.enteredAmount) {
+      // TODO create alert!!!
+      console.log('Please enter a valid reason and amount!');
+      return;
+    }
+    if (this.enteredReason.trim().length <= 0 || +this.enteredAmount <= 0) {
+      // TODO create alert!!!
+      console.log('Please enter a valid reason and amount!');
+      return;
+    }
+    console.log('Valid!!!');
+  }
 }
